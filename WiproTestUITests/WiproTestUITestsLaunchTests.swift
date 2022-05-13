507185ce92c0e26d5ce79468e8e1e9eb127f7d28
Nitin <nunda@allstate.com>
2022-05-13 09:54:54 +0530

    Main: Initial setup

---
+++ WiproTestUITests/WiproTestUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  WiproTestUITestsLaunchTests.swift
+//  WiproTestUITests
+//
+//  Created by Undare, Nitin Namadeo (INFOSYS) on 13/05/22.
+//
+
+import XCTest
+
+class WiproTestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
