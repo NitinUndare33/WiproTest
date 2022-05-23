386ac20c74d976e651e5fd7f6b6a974ef95c44c2
ganesh padole <ganeshpadole55@gmail.com>
2022-05-24 00:31:12 +0530

    Added test cases

--- WiproTestTests/WiproTestTests.swift
+++ WiproTestTests/WiproTestTests.swift
@@ -12,140 +12,54 @@ import XCTest
 class WiproTestTests: XCTestCase {
     var controller: ViewController!
     var viewControllerMirror: ViewControllerMirror!
-    var tableView = UITableView()
-//
-//    override func setUp() {
-//        super.setUp()
-//        setUpViewController()
-//        viewControllerMirror = ViewControllerMirror(viewController: controller)
-//        tableView = viewControllerMirror.tableView ?? UITableView()
-//    }
+    var tableView: UITableView?
     
-//    func setUpViewController() {
-//        let stroyBoard = AllstateEnvironmentSelectionController.storyBoard
-//        controller = stroyBoard.instantiateViewController(withIdentifier: "EnvSelectionVC") as? AllstateEnvironmentSelectionController
-//        controller.beginAppearanceTransition(true, animated: false)
-//        controller.endAppearanceTransition()
-//    }
-//
-//    func testBuildSections() {
-//        let section = EnvironmentSwitchSection.buildSections()
-//        XCTAssertNotNil(section)
-//    }
-//
-//    func testReduce() {
-//        guard let sections = EnvironmentSwitchSection.buildSections() else {
-//            return
-//        }
-//        let section = EnvironmentSwitchSection.reduce(from: sections, model: .milewise, shouldExpand: true)
-//        XCTAssertTrue(section.contains { $0.headerIdentifier == AllstateEnvironmentSelectionHeader.identifier })
-//    }
-//
-//    func testNumberOfSections() {
-//        // Act & Arrange
-//        XCTAssertNotNil(controller.numberOfSections(in: tableView))
-//        let sections = tableView.numberOfSections
-//        // Assert
-//        XCTAssert(sections == 1, "Failed: Number of sections is not two")
-//    }
-//
-//    func testCellForRowAt() {
-//        let indexPath = IndexPath(row: 0, section: 0)
-//        XCTAssertNotNil(controller.tableView(tableView, cellForRowAt: indexPath))
-//    }
-//
-//    func testNumberOfRowsInSection() {
-//        guard let sections = EnvironmentSwitchSection.buildSections() else {
-//            return
-//        }
-//        XCTAssertNotNil(controller.tableView(tableView, numberOfRowsInSection: sections.count - 1))
-//    }
-//
-//    func testSetInitialLaunch() {
-//        XCTAssertNotNil(controller.setIntialLaunch(key: Configuration.initialLaunchKey))
-//    }
-//
-//    func testHeightForHeaderInSection() {
-//        let height: CGFloat = 45
-//        XCTAssertEqual(height, controller.tableView(tableView, heightForHeaderInSection: 45))
-//    }
-//
-//    func testNumberOfRows() {
-//        controller.viewDidLoad()
-//        XCTAssertNotNil(tableView.numberOfRows(inSection: 0))
-//    }
-//
-//    func testHeightForRow() {
-//        let indexPath = IndexPath(row: 0, section: 0)
-//        let height = controller.tableView(tableView, heightForRowAt: indexPath) == 80.0
-//        XCTAssertTrue(height)
-//    }
-//
-//    func testDatePicker_SetUp_ShouldSucceed() {
-//        controller.setupDatePicker()
-//        let datePicker = controller.datePickerView
-//        XCTAssertNotNil(datePicker)
-//        XCTAssertEqual(datePicker?.datePickerMode, .date)
-//        XCTAssertNotNil(datePicker?.minimumDate)
-//    }
-//
-//    func testDateTextFeild_ShouldSucceed() {
-//        controller.setupDatePicker()
-//        let textField = viewControllerMirror.systemDateTextField
-//        XCTAssertNotNil(textField)
-//        XCTAssertEqual(textField?.inputView, controller.datePickerView)
-//        XCTAssertEqual(textField?.tintColor, .clear)
-//        if let previousDate = UserDefaults.standard.string(forKey: "SelectedSystemDate") {
-//            XCTAssertEqual(textField?.text, "Test System Date (Debug): \(previousDate)")
-//        } else {
-//            let date = controller.dateFormatter.string(from: Date())
-//            XCTAssertEqual(textField?.text, "Test System Date (Debug): \(date)")
-//        }
-//        XCTAssertEqual(textField?.inputAccessoryView, controller.toolBar)
-//    }
-//
-//    func testDateTextFeild_RightView_ShouldSucceed() {
-//        controller.setUpTextFeildImage()
-//        XCTAssertEqual(viewControllerMirror.systemDateTextField?.rightViewMode, .always)
-//        XCTAssertNotNil(viewControllerMirror.systemDateTextField?.rightView)
-//    }
-//
-//    func testToolBar_ShouldSucceed() {
-//        controller.createUIToolBar()
-//        let toolBar = controller.toolBar
-//        XCTAssertNotNil(toolBar)
-//        XCTAssertEqual(toolBar?.barStyle, .default)
-//        XCTAssertTrue(toolBar?.isTranslucent ?? false)
-//        XCTAssertEqual(toolBar?.tintColor, .white)
-//        XCTAssertEqual(toolBar?.items?.count, 3)
-//    }
+    override func setUp() {
+        super.setUp()
+        setUpViewController()
+        viewControllerMirror = ViewControllerMirror(viewController: controller)
+        tableView = viewControllerMirror.tableView ?? UITableView()
+    }
     
-}
-
-protocol Reflection {
-    var mirror: Mirror { get set }
+    func setUpViewController() {
+        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+        controller = storyBoard.instantiateViewController(withIdentifier: "ViewController") as? ViewController
+        controller.beginAppearanceTransition(true, animated: false)
+        controller.endAppearanceTransition()
+    }
     
-    func extract<T>(variableName: StaticString) -> T?
-}
-
-extension Reflection {
-    func extract<T>(variableName: StaticString = #function) -> T? {
-        mirror.descendant("\(variableName)") as? T
+    func test_Objects_ShoudNotNull() {
+        XCTAssertNotNil(controller)
+        XCTAssertNotNil(viewControllerMirror)
+        XCTAssertNotNil(tableView)
+    }
+    
+    func test_APICall_ShouldSucceed() {
+        controller.viewModel?.getUserDetails()
+        let exp = expectation(description: "Test after 5 seconds")
+        let result = XCTWaiter.wait(for: [exp], timeout: 5.0)
+        if result == XCTWaiter.Result.timedOut {
+            if let result = self.controller.viewModel?.userData {
+                XCTAssertNotNil(result)
+                XCTAssertNotNil(result.userTitle)
+                XCTAssertNotNil(result.userInfo)
+            }
+        } else {
+            XCTFail("Delay interrupted")
+        }
+    }
+    
+    func test_EndPointURL_ShoulSucced() {
+        let endPoint = URL(string: APIEndPoints.userAPI)
+        XCTAssertNotNil(endPoint)
+    }
+    
+    func test_Constant_ShouldSucced() {
+        XCTAssertEqual(ErrorMessage.apiDown, "Something went wrong")
+        XCTAssertEqual(APIEndPoints.userAPI, "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json")
     }
 }
 
-class ViewControllerMirror: Reflection {
-    var mirror: Mirror
-    
-    init(viewController: UIViewController) {
-        mirror = Mirror(reflecting: viewController)
-    }
-}
-
-class TableViewCellMirror: Reflection {
-    var mirror: Mirror
-    
-    init(tableViewCell: UITableViewCell) {
-        mirror = Mirror(reflecting: tableViewCell)
-    }
+extension ViewControllerMirror {
+    var tableView: UITableView? { return extract() }
 }
